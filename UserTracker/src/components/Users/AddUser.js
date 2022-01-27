25c72ea132bb6e4cd14ee357f2c11f8631686314
Colton <coltonfalkner@gmail.com>
2022-01-27 15:06:34 -0500

    pre-logic

--- UserTracker/src/components/Users/AddUser.js
+++ UserTracker/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import classes from './AddUser.module.css'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
+import ErrorModal from '../UI/ErrorModal'
 
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState('')
@@ -15,7 +16,7 @@ const AddUser = (props) => {
     if (+enteredAge < 1) {
       return
     }
-    console.log(enteredUserName, enteredAge)
+    props.onAddUser(enteredUserName, enteredAge)
     setEnteredUserName('')
     setEnteredAge('')
   }
@@ -28,25 +29,28 @@ const AddUser = (props) => {
   }
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input
-          id='username'
-          type='text'
-          value={enteredUserName}
-          onChange={usernameChangeHandler}
-        />
-        <label htmlFor='age'>Age (Years)</label>
-        <input
-          id='age'
-          type='number'
-          value={enteredAge}
-          onChange={ageChangeHandler}
-        />
-        <Button type='submit'>Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title='An error occurred!' message='Something went wrong!' />
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor='username'>Username</label>
+          <input
+            id='username'
+            type='text'
+            value={enteredUserName}
+            onChange={usernameChangeHandler}
+          />
+          <label htmlFor='age'>Age (Years)</label>
+          <input
+            id='age'
+            type='number'
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+    </div>
   )
 }
 
