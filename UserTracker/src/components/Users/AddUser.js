e122daf4bc8495b5f6bec1a52f3fc5fd4d87c847
Colton <coltonfalkner@gmail.com>
2022-01-27 15:18:16 -0500

    modal logic implemted

--- UserTracker/src/components/Users/AddUser.js
+++ UserTracker/src/components/Users/AddUser.js
@@ -7,13 +7,23 @@ import ErrorModal from '../UI/ErrorModal'
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState('')
   const [enteredAge, setEnteredAge] = useState('')
+  const [error, setError] = useState()
 
   const addUserHandler = (event) => {
     event.preventDefault()
     if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message:
+          'Please enter a valid name and age (cannot submit empty values)',
+      })
       return
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (must be greater than 0)',
+      })
       return
     }
     props.onAddUser(enteredUserName, enteredAge)
@@ -28,9 +38,19 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value)
   }
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
     <div>
-      <ErrorModal title='An error occurred!' message='Something went wrong!' />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
